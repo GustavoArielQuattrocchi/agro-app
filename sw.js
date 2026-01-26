6920f1468a2ffe85795cca49e972a7d0e785ec4c
GustavoArielQuattrocchi <166957133+GustavoArielQuattrocchi@users.noreply.github.com>
2026-01-26 15:17:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "orden-cura-v14";
+const CACHE_NAME = "orden-cura-v15";
 const ASSETS = [
   "/",
   "/index.html",
@@ -39,6 +39,7 @@ self.addEventListener("fetch", (e) => {
   );
 
 });
+
 
 
 
